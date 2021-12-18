ce31f064750a3100172cd3209f28c4c7aaa3ebb8
Halvorsen, Rikard <Rikard.Halvorsen@storebrand.no>
2021-12-18 13:42:33 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# eidsprinten
+# Eidsprinten
+
+## Run locally
+
+Start EidsprintenApplication and go to http://localhost:8080/eidsprinten/swagger-ui/
---
+++ src/main/java/com/halrik/eidsprinten/EidsprintenApplication.java
@@ -0,0 +1,13 @@
+package com.halrik.eidsprinten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EidsprintenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EidsprintenApplication.class, args);
+	}
+
+}
