5fc16da420d534a50271e12c9705c1cd340356eb
Halvorsen, Rikard <Rikard.Halvorsen@storebrand.no>
2022-02-09 15:48:47 +0100

    sql statements with changes done so far

--- SQL statements.sql
+++ SQL statements.sql
@@ -1,1 +1,11 @@
-select * from participant where first_name = 'Navn';
+select * from team where (participant_leg1name = 'Navn Løs' or participant_leg2name = 'Navn Løs') order by club_name;
+
+select * from team where bib = 167;
+select * from participant where id = 293;
+update participant set first_name = 'Jonas', last_name = 'Walle' where id = 293;
+update team set participant_leg1name = 'Jonas Walle' where bib = 167;
+
+select * from team where bib = 72;
+select * from participant where id = 8;
+update participant set first_name = 'Ola Jacobsen', last_name = 'Myklatun' where id = 8;
+update team set participant_leg1name = 'Ola Jacobsen Myklatun' where bib = 72;
